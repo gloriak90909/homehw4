63ba7a8ac5ad2269e5c44c154cd6b15f74845c51
Gloria Kim <gloriak90909@gmail.com>
2025-11-07 15:23:18 -0500

    implemented shift and unshift rows in aes.c

--- aes.c
+++ aes.c
@@ -6,7 +6,7 @@
  */
 
 #include "aes.h"
-#include <aesUtil.h>
+#include "aesUtil.h"
 
 
 /**
@@ -42,11 +42,11 @@ void blockToSquare( byte square[ BLOCK_ROWS ][ BLOCK_COLS ], byte const data[ BL
   //d h l p
 
   //index = 6   g  [2][1] = data[ 1 * 4 + 2 ]  
-  //square[r][c] = data[ c * BLOCK_ROWS + r ]
+  //square[r][c] = data[ c * BLOCK_ROWS + i ]
 
-    for (int r = 0; r < BLOCK_ROWS; r++) {
+    for (int i = 0; i < BLOCK_ROWS; i++) {
         for (int c = 0; c < BLOCK_COLS; c++) {
-            square[r][c] = data[r + (BLOCK_ROWS *c)];
+            square[i][c] = data[i + (BLOCK_ROWS *c)];
 
         }
     }
@@ -59,9 +59,10 @@ void blockToSquare( byte square[ BLOCK_ROWS ][ BLOCK_COLS ], byte const data[ BL
  * @param square 4x4 array representing the current AES state 
  */
 void squareToBlock( byte data[ BLOCK_SIZE ], byte const square[ BLOCK_ROWS ][ BLOCK_COLS ] ) {
-    for (int r = 0; r < BLOCK_ROWS; r++) {
-        for (int c = 0; c < BLOCK_COLS; c++) {
-            data[r + (BLOCK_ROWS * c)] = square[r][c];
+    for (int i = 0; i < BLOCK_ROWS; i++) {
+        for (int col = 0; col < BLOCK_COLS; col++) {
+            data[i + (BLOCK_ROWS * col)] = square[i][col];
+
         }
     }
 }
@@ -71,6 +72,47 @@ void squareToBlock( byte data[ BLOCK_SIZE ], byte const square[ BLOCK_ROWS ][ BL
  * @param square 4x4 array representing the current AES state
  */
 void shiftRows( byte square[ BLOCK_ROWS ][ BLOCK_COLS ] ) {
+    //a e i m
+    //b f j n
+    //c g k o
+    //d h l p
+
+    //shift row 0 left by 0
+    //shift row 1 left by 1
+    //shift row 2 left by 2
+    //shift row 3 left by 3
+
+    //a e i m
+    //f j N b
+    //k o c g
+    //p d h l
+
+    ////shift row 1 left by 1
+    
+    //b f j n  ->  F j n b 
+    //2nd row (index 1)
+    byte tone = square[1][0]; //b 
+    square[1][0] = square[1][1]; //b = f
+    square[1][1] = square[1][2]; //f = j
+    square[1][2] = square[1][3];        
+    square[1][3] = tone;
+
+    //c g k o  ->  k o c g
+    //shift row 2 left by 2
+    byte ttwo = square[2][0]; //c
+    byte ttwo1 = square[2][1]; //g
+    square[2][0] = square[2][2]; //c = k
+    square[2][1] = square[2][3]; //g = o
+    square[2][2] = ttwo; //k = c
+    square[2][3] = ttwo1; //o = g 
+
+    //d h l p  ->  p d h l
+    //shift row 3 left by 3
+    byte tthree = square[3][3]; //p
+    square[3][3] = square[3][2]; //p = l    
+    square[3][2] = square[3][1]; //l = h
+    square[3][1] = square[3][0]; //h = d
+    square[3][0] = tthree; //d = p      
 
 }
 
@@ -79,6 +121,39 @@ void shiftRows( byte square[ BLOCK_ROWS ][ BLOCK_COLS ] ) {
   * @param square 4x4 array representing the current AES state
  */
 void unShiftRows( byte square[ BLOCK_ROWS ][ BLOCK_COLS ] ) {
+    //a e i m
+    //f j N b
+    //k o c g
+    //p d h l
+
+    //a e i m
+    //b f j n
+    //c g k o
+    //d h l p
+
+    //row 1 right by 1
+    byte tone = square[1][3]; //b
+    square[1][3] = square[1][2]; //b = n
+    square[1][2] = square[1][1]; //n = j      
+    square[1][1] = square[1][0];
+    square[1][0] = tone;
+    
+    //row 2 right by 2
+    byte ttwo = square[2][3]; //g
+    byte ttwo1 = square[2][2]; //c
+    square[2][3] = square[2][1]; //g = o
+    square[2][2] = square[2][0]; //c = k    
+    square[2][1] = ttwo1; //o = c
+    square[2][0] = ttwo; //k = g
+    
+    //row 3 right by 3
+    byte tthree = square[3][0]; //d
+    square[3][0] = square[3][1]; //d = p    
+    square[3][1] = square[3][2]; //p = h  
+    square[3][2] = square[3][3]; //h = l
+    square[3][3] = tthree; //l = d
+
+
 
 }
 
